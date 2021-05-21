4a68376911c380b398041bcde722961e1a1f01f7
leeseast <leeseast27@gmail.com>
2021-05-21 16:14:15 +0800

    init

---
+++ study-validation/src/main/java/com/leeseast2Z/validation/Main.java
@@ -0,0 +1,12 @@
+package com.leeseast2Z.validation;
+
+/**
+ * @Author: limf
+ * @Date: 2021/5/21 16:09
+ * @Description: Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hi~");
+    }
+}
